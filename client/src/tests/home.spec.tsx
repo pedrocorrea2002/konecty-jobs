47b9de59c19523338156ca25aa5cc611671db0ad
pedrocorrea2002 <pedro.hcorrea15@gmail.com>
2024-03-31 21:55:41 -0300

    Fazendo o teste unitário parar de dar o erro de "Cannot use import statement outside a module"

--- client/src/tests/home.spec.tsx
+++ client/src/tests/home.spec.tsx
@@ -2,17 +2,20 @@ import {describe,expect,test} from "@jest/globals"
 import {render, screen} from "@testing-library/react"
 import Home from "@/app/page"
 
-describe("Testando se os produtos estão vindo corretamente"", () => {
+describe("testando se o número de produtos na tela é o mesmo da base de dados", () => {
     test("testando se o número de produtos na tela é o mesmo da base de dados",() => {
         render(<Home/>)
         const produtos = screen.getAllByTestId("produto")
         
         expect(produtos.length).toBe(30)
      })
-     test("testando se o número de produtos na tela com o texto 'pa' é o mesmo da base de dados", () => {
-        render(<Home/>)
-        const produtos = screen.getAllByTestId("produto")
-         
-        expect(produtos.filter(item => item.text.toLowerCase().match('pa')).length).toBe(3)
-     })
+})
+
+describe("testando se o número de produtos na tela com o texto 'pa' é o mesmo da base de dados",() => {
+   test("testando se o número de produtos na tela com o texto 'pa' é o mesmo da base de dados", () => {
+      render(<Home/>)
+      const produtos = screen.getAllByTestId("produto")
+       
+      expect(produtos.filter(item => item.text.toLowerCase().match('pa')).length).toBe(3)
+   })
 })
