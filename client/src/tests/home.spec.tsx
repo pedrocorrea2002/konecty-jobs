4a43d80d1650bb8dbea5448675966b7fd02df60d
pedrocorrea2002 <pedro.hcorrea15@gmail.com>
2024-03-31 23:15:33 -0300

    testando o teste automatizado

--- client/src/tests/home.spec.tsx
+++ client/src/tests/home.spec.tsx
@@ -2,20 +2,37 @@ import {describe,expect,test} from "@jest/globals"
 import {render, screen} from "@testing-library/react"
 import Home from "@/app/page"
 
-describe("testando se o número de produtos na tela é o mesmo da base de dados", () => {
-    test("testando se o número de produtos na tela é o mesmo da base de dados",() => {
-        render(<Home/>)
-        const produtos = screen.getAllByTestId("produto")
-        
-        expect(produtos.length).toBe(30)
-     })
+describe("comprar produto", () => {
+   test("botão de comprar",() => {
+      
+   }),
+   test("verificar se o cliente está logado",() => {
+      
+   }),
+   test("verificar se o endereço do cliente está cadastrado",() => {
+      expect(2).toBe(3)
+   }),
+   test("cadastrando endereço se não houver",() => {
+      
+   }),
+   test("preenchendo cartão",() => {
+      
+   })
 })
-
-describe("testando se o número de produtos na tela com o texto 'pa' é o mesmo da base de dados",() => {
-   test("testando se o número de produtos na tela com o texto 'pa' é o mesmo da base de dados", () => {
-      render(<Home/>)
-      const produtos = screen.getAllByTestId("produto")
-       
-      expect(produtos.filter(item => item.text.toLowerCase().match('pa')).length).toBe(3)
+describe("alugar produto", () => {
+   test("botão de comprar",() => {
+      
+   }),
+   test("verificar se o cliente está logado",() => {
+      
+   }),
+   test("verificar se o endereço do cliente está cadastrado",() => {
+      
+   }),
+   test("cadastrando endereço se não houver",() => {
+      
+   }),
+   test("preenchendo cartão",() => {
+      expect(2).toBe(3)
    })
 })
